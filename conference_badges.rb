be760fbb90f7780aa8561f7a56e553208d0cd37a
Roberto Samia <bojosteph@gmail.com>
2018-10-25 04:09:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+  
+end
+
+
+def batch_badge_creator(attendees)
+  
+  attendees.each do |name|
+   badges ==  "Hello, my name is #{name}."
+  end
+  badges
+end
